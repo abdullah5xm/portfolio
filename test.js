be450c2ccab95aa0332aa9017bfd89445e1ced8a
Samrat <67840234+samrat9xm@users.noreply.github.com>
2021-10-08 09:19:39 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+///////  menu
+    let n = 0;
+    function mnu() {
+      if (n == 0) {
+        document.getElementById('id1').style.height = '350px';
+        document.getElementById('id2').innerHTML =
+        '<i class="fas fa-times"></i>';
+
+        n = 1;
+      } else if (n == 1) {
+        document.getElementById('id1').style.height = '70px';
+        document.getElementById('id2').innerHTML =
+        '<i class="fas fa-hamburger"></i>';
+        n = 0;
+      }
+    }
